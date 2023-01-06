bb0c5adc5d696ceb8bdf237765a833ac8a8bf044
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-01-06 19:31:22 +0100

    style: reindent

--- Javascript.js
+++ Javascript.js
@@ -2,59 +2,59 @@ window.addEventListener("keydown", checkKey, false);
 window.onload = ajax;
 
 function checkKey(key) {
-   if (key.keyCode === 13) {
-      answer();
-   }
+  if (key.keyCode === 13) {
+    answer();
+  }
 }
 function wordToSpeech() {
-   const phoneticAnswer = document.getElementById("phoneticAnswer");
-   responsiveVoice.speak(phoneticAnswer.value, "Vietnamese Female");
+  const phoneticAnswer = document.getElementById("phoneticAnswer");
+  responsiveVoice.speak(phoneticAnswer.value, "Vietnamese Female");
 }
 function textToSpeech() {
-   const text = document.getElementById("vietnameseText");
-   responsiveVoice.speak(text.value, "Vietnamese Female", { rate: 0.75 });
+  const text = document.getElementById("vietnameseText");
+  responsiveVoice.speak(text.value, "Vietnamese Female", { rate: 0.75 });
 }
 
 function answer() {
-   const userInput = document.getElementById("inputField");
-   const correctAnswer = document.getElementById("answer");
-   const phoneticAnswer = document.getElementById("phoneticAnswer");
-   if (userInput.value === correctAnswer.value) {
-      userInput.style.color = "black";
-      userInput.style.backgroundColor = "lightgreen";
-      userInput.value = phoneticAnswer.value;
-      setTimeout(ajax, 1000);
-   } else {
-      userInput.style.color = "red";
-      userInput.placeholder = phoneticAnswer.value;
-      userInput.value = "";
-   }
+  const userInput = document.getElementById("inputField");
+  const correctAnswer = document.getElementById("answer");
+  const phoneticAnswer = document.getElementById("phoneticAnswer");
+  if (userInput.value === correctAnswer.value) {
+    userInput.style.color = "black";
+    userInput.style.backgroundColor = "lightgreen";
+    userInput.value = phoneticAnswer.value;
+    setTimeout(ajax, 1000);
+  } else {
+    userInput.style.color = "red";
+    userInput.placeholder = phoneticAnswer.value;
+    userInput.value = "";
+  }
 }
 
 function ajax() {
-   const correctAnswer = document.getElementById("answer");
-   const phoneticAnswer = document.getElementById("phoneticAnswer");
-   const sentence = document.getElementById("sentence");
-   const englishWord = document.getElementById("englishWord");
-   const englishText = document.getElementById("englishText");
-   const vietnameseText = document.getElementById("vietnameseText");
-   const id = document.getElementById("id");
-   let xml = new XMLHttpRequest();
-   xml.open("GET", "AJAX.php?id=" + id.innerHTML, true);
-   xml.onload = function() {
-      if (this.responseText === "400") {
-         sentence.innerHTML = "Problem with connection. Error 400.";
-      } else {
-         let arr = JSON.parse(this.responseText);
-         sentence.innerHTML = arr.vietnamese_text;
-         correctAnswer.value = arr.simple_word;
-         phoneticAnswer.value = arr.vietnamese_word;
-         id.innerHTML = arr.id;
-         englishWord.innerHTML = arr.english_word;
-         englishText.innerHTML = arr.english_text;
-         phoneticAnswer.value = arr.vietnamese_word;
-         vietnameseText.value = arr.fullText;
-      }
-   };
-   xml.send();
+  const correctAnswer = document.getElementById("answer");
+  const phoneticAnswer = document.getElementById("phoneticAnswer");
+  const sentence = document.getElementById("sentence");
+  const englishWord = document.getElementById("englishWord");
+  const englishText = document.getElementById("englishText");
+  const vietnameseText = document.getElementById("vietnameseText");
+  const id = document.getElementById("id");
+  let xml = new XMLHttpRequest();
+  xml.open("GET", "AJAX.php?id=" + id.innerHTML, true);
+  xml.onload = function () {
+    if (this.responseText === "400") {
+      sentence.innerHTML = "Problem with connection. Error 400.";
+    } else {
+      let arr = JSON.parse(this.responseText);
+      sentence.innerHTML = arr.vietnamese_text;
+      correctAnswer.value = arr.simple_word;
+      phoneticAnswer.value = arr.vietnamese_word;
+      id.innerHTML = arr.id;
+      englishWord.innerHTML = arr.english_word;
+      englishText.innerHTML = arr.english_text;
+      phoneticAnswer.value = arr.vietnamese_word;
+      vietnameseText.value = arr.fullText;
+    }
+  };
+  xml.send();
 }
--- index.php
+++ index.php
@@ -6,31 +6,31 @@
 <link rel="stylesheet" href="CSS.css">
 
 <body>
-   <div class="center">
-      <div class="card" style="height: 50px; line-height: 50px; border-top-left-radius: 15px; border-top-right-radius: 15px; background-color: #F5F6F9;    padding-top: 10px; padding-bottom: 10px;font-size: 30px; font-weight: bold;">Vietnamist</div>
-      <div class="card" style="border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;box-shadow: 6px 8px rgba(100, 100, 100, 0.3);">
-         <div id='id'></div>
-         <div id='englishWord'></div>
-         <br>
-         <div class='sentence' id='sentence'></div>
-         <p id="englishText"></p>
-         <input id='answer' hidden>
-         <input id='phoneticAnswer' hidden>
-         <input id='vietnameseText' hidden>
-         <br>
-         <button onclick="wordToSpeech()">Play answer</button>
-         <button onclick="textToSpeech()">Play sentence</button>
-      </div>
-   </div>
-   <!--- End of class "center" --->
-   <script src="Javascript.js"></script>
-   <?php
-   // Load environment API key for 'responsivevoice.org'
-   require 'vendor/autoload.php';
-   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-   $dotenv->load();
-   ?>
-   <script src="https://code.responsivevoice.org/responsivevoice.js?key=<?php echo $_ENV['VOICE_KEY']; ?>"></script>
+    <div class="center">
+        <div class="card" style="height: 50px; line-height: 50px; border-top-left-radius: 15px; border-top-right-radius: 15px; background-color: #F5F6F9;    padding-top: 10px; padding-bottom: 10px;font-size: 30px; font-weight: bold;">Vietnamist</div>
+        <div class="card" style="border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;box-shadow: 6px 8px rgba(100, 100, 100, 0.3);">
+            <div id='id'></div>
+            <div id='englishWord'></div>
+            <br>
+            <div class='sentence' id='sentence'></div>
+            <p id="englishText"></p>
+            <input id='answer' hidden>
+            <input id='phoneticAnswer' hidden>
+            <input id='vietnameseText' hidden>
+            <br>
+            <button onclick="wordToSpeech()">Play answer</button>
+            <button onclick="textToSpeech()">Play sentence</button>
+        </div>
+    </div>
+    <!--- End of class "center" --->
+    <script src="Javascript.js"></script>
+    <?php
+    // Load environment API key for 'responsivevoice.org'
+    require 'vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    ?>
+    <script src="https://code.responsivevoice.org/responsivevoice.js?key=<?php echo $_ENV['VOICE_KEY']; ?>"></script>
 </body>
 
 </html>
